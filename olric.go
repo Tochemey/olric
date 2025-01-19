a4eeee8e1493ca576d451f13d0f5c02c1c0b67d8
Tochemey <tochemey@hotmail.com>
2025-01-19 13:02:07 +0000

    refactor: change release version appropriately

--- olric.go
+++ olric.go
@@ -61,7 +61,7 @@ import (
 )
 
 // ReleaseVersion is the current stable version of Olric
-const ReleaseVersion string = "0.6.0-alpha.1"
+const ReleaseVersion string = "0.2.0-alpha"
 
 var (
 	// ErrOperationTimeout is returned when an operation times out.
