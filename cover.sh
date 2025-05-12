9d9bb8b0a5de627bcfc8d3c0a055b4d103d728b7
Tochemey <tochemey@hotmail.com>
2025-05-12 18:59:18 +0100

    refactor: maintenance

--- cover.sh
+++ cover.sh
@@ -28,7 +28,7 @@ set -e
 echo 'mode: atomic' > $OUT
 for PKG in $(go list ./...| grep -v -E 'vendor'|grep -v -E 'hasher'|grep -v -E 'internal/bufpool'|
 grep -v -E 'internal/flog'|grep -v -E 'serializer'|grep -v -E 'stats'|grep -v -E 'cmd'); do
-  go test -v -covermode=atomic -coverprofile=$TMP $PKG
+  go test -v -p 1 -covermode=atomic -coverprofile=$TMP $PKG
   tail -n +2 $TMP >> $OUT
 done
 
