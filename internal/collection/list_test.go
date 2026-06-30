af57e53216f6057e0e75bface0c6541fdcb426f4
Tochemey <tochemey@hotmail.com>
2026-06-30 10:15:10 +0100

    chore: update license headers

--- internal/collection/list_test.go
+++ internal/collection/list_test.go
@@ -1,5 +1,6 @@
 /*
- * Copyright 2025 Arsene Tochemey Gandote
+ * Copyright 2018-2024 Burak Sezer
+ * Copyright 2025-2026 Arsene Tochemey Gandote
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- internal/syncstate/syncstate.go
+++ internal/syncstate/syncstate.go
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2018-2024 Burak Sezer
  * Copyright 2025-2026 Arsene Tochemey Gandote
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
